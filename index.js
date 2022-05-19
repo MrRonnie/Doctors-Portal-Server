439b54344af5b8a00ca556a01f057a021d9bf22d
MrRonnie <ronymaza55@gmail.com>
2022-05-19 17:36:26 +0600

    Display User specific Appointments using Data Table

--- index.js
+++ index.js
@@ -44,27 +44,11 @@ async function run() {
      * app.delete('/booking/:id') //
      */
 
-    app.get("/available", async (req, res) => {
-      const date = req.query.date || "May 11, 2022";
-
-      // step 1 : get all services
-      const services = await serviceCollection.find().toArray();
-
-      // step 2 : get the booking of the day
-      const query = { date: date };
+    app.get("/booking", async (req, res) => {
+      const patient = req.query.patient;
+      const query = { patient: patient };
       const bookings = await bookingCollection.find(query).toArray();
-
-      // step 3 : for each service find bookings for the service
-      services.forEach((service) => {
-        const serviceBookings = bookings.filter(
-          (b) => b.treatment === service.name
-        );
-        const booked = serviceBookings.map((s) => s.slot);
-        const available = service.slots.filter((s) => !booked.includes(s));
-        service.available = available;
-      });
-
-      res.send(services);
+      res.send(bookings);
     });
 
     app.post("/booking", async (req, res) => {
@@ -80,6 +64,36 @@ async function run() {
       }
       const result = await bookingCollection.insertOne(booking);
       return res.send({ success: true, result });
+    });
+
+    // Warning: This is not the proper way!!
+
+    app.get("/available", async (req, res) => {
+      const date = req.query.date || "May 11, 2022";
+
+      // step 1:  get all services
+      const services = await serviceCollection.find().toArray();
+
+      // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}, {}]
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+
+      // step 3: for each service
+      services.forEach((service) => {
+        // step 4: find bookings for that service. output: [{}, {}, {}, {}]
+        const serviceBookings = bookings.filter(
+          (book) => book.treatment === service.name
+        );
+        // step 5: select slots for the service Bookings: ['', '', '', '']
+        const bookedSlots = serviceBookings.map((book) => book.slot);
+        // step 6: select those slots that are not in bookedSlots
+        const available = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        //step 7: set available to slots to make it easier
+        service.slots = available;
+      });
+      res.send(services);
     });
 
     // ---------------
